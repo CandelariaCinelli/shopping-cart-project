623787bbc4112de4d3a94983b74c1707024d4ea5
Candelaria Cinelli <caande.cba@gmail.com>
2026-04-21 19:51:37 -0300

    first commit - shopping cart project

---
+++ src/data/products.js
@@ -0,0 +1,39 @@
+export const products = [
+    {
+        id: 1,
+        name: "Teclado Mecanico RGB",
+        description:"Teclado Mecanico con luces RGB switches cherry red",
+        price: 1000
+    },
+
+    {
+        id: 2,
+        name: "Samsung Smart Tv 55",
+        description:"Excelente tv LCD...",
+        price: 3000
+    },
+    {
+        id: 3,
+        name: "Audifono Bluetooth Sony",
+        description:"Audifono Bluetooth para Juegos",
+        price: 600
+    },
+    {
+        id: 4,
+        name: "Memoria Corsair 8GB DDR5",
+        description:"Memoria RAM optimizada para juegos",
+        price: 4000
+    },
+    {
+        id: 5,
+        name: "Asus Nvidia RTX",
+        description:"Tarjeta Gráfica para juegos 4k",
+        price: 800
+    },
+    {
+        id: 6,
+        name: "Cpu Intel Core i5",
+        description:"Optimixado para tareas multicore",
+        price: 8000
+    },
+]
